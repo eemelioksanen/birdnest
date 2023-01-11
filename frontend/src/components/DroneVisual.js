30fe61d22c571bf68636621aa95f14fee179e479
Eemeli <eemeli.oksanen@aalto.fi>
2023-01-11 20:19:27 +0200

    add styling for offender form

--- frontend/src/components/DroneVisual.js
+++ frontend/src/components/DroneVisual.js
@@ -1,23 +1,45 @@
 import { useSelector } from 'react-redux'
-import { Stage, Layer, Circle, Text, Rect, Group, Image } from 'react-konva'
+import { Stage, Layer, Circle, Text, Rect, Group } from 'react-konva'
 
 const originX = 285
 const originY = 285
 
 const Drone = ({ data }) => {
+  const dataPosX = Number(data.positionX._text) / 1000
+  const dataPosY = Number(data.positionY._text) / 1000
+  let textPosX
+  let textPosY
+
+  // prevent text from going over the border in horizontal direction
+  if (dataPosX < 50) {
+    textPosX = 50
+  } else if (dataPosX > 440) {
+    textPosX = 440
+  } else {
+    textPosX = originX + dataPosX - 250 - 45
+  }
+
+  // prevent text from going over the border in vertical direction
+  if (dataPosY < 50) {
+    textPosY = 510 - dataPosY
+  } else {
+    textPosY = originY - dataPosY + 270
+  }
+
   return (
     <Group>
       <Circle
-        x={originX + Number(data.positionX._text) / 1000 - 250}
-        y={originY - Number(data.positionY._text) / 1000 + 250}
+        x={originX + dataPosX - 250}
+        y={originY - dataPosY + 250}
         radius={5}
         fill='grey'
         stroke='white'
       />
       <Text
-        text={data.serialNumber._text}
-        x={originX + Number(data.positionX._text) / 1000 - 250 - 45}
-        y={originY - Number(data.positionY._text) / 1000 + 250 + 20}
+        text={`${data.serialNumber._text}
+        ${Math.floor(dataPosX)}, ${Math.floor(dataPosY)}`}
+        x={textPosX}
+        y={textPosY}
         fill='white'
       />
     </Group>
@@ -42,6 +64,7 @@ const Background = () => {
           y: size * backRectMultiplier,
         }}
         fillLinearGradientColorStops={[0, 'lightblue', 1, 'lightgreen']}
+        cornerRadius={10}
       />
       <Text text='0, 0' x={originX - 270} y={originY + 255} fill='black' />
       <Text text='0, 500' x={originX - 270} y={originY - 265} fill='black' />
@@ -55,6 +78,7 @@ const Background = () => {
         fill='#1d1e30'
         stroke='black'
         shadowBlur={20}
+        cornerRadius={10}
       />
     </Group>
   )
@@ -69,17 +93,19 @@ const Duck = () => {
   return <Circle x={originX} y={originY} radius={5} fill='red' stroke='white' />
 }
 
-const style = {
+const droneVisualStyle = {
   display: 'inline-block',
   color: 'white',
-  width: '50%',
+  width: '750px',
+  height: '580px',
   float: 'left',
+  animation: 'Fade 1s linear',
 }
 
 const DroneVisual = () => {
   const drones = useSelector((state) => state.drones)
   return (
-    <div style={style}>
+    <div style={droneVisualStyle}>
       <Stage width={700} height={570}>
         <Layer>
           <Background />
