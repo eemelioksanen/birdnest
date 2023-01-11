058a549f75b71ef3b9fe15b87dd4a69fe2bbb40f
Eemeli <eemeli.oksanen@aalto.fi>
2023-01-11 23:44:48 +0200

    change getData() fallback return value from null to empty array

--- frontend/src/services/droneService.js
+++ frontend/src/services/droneService.js
@@ -2,10 +2,12 @@ const droneUrl = '/api/drones'
 const offenderUrl = '/api/offenders'
 
 const getData = async (url) => {
-  return fetch(url).then((response) => {
-    if (response.status !== 200) return null
-    return response.json()
-  })
+  return fetch(url)
+    .then((response) => {
+      if (response.status !== 200) return []
+      return response.json()
+    })
+    .catch((error) => console.log(error.message))
 }
 
 const getOffenders = () => getData(offenderUrl)
