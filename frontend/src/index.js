2cfcb418d14c61c60102876e0baea2adf2987b7a
Eemeli <eemeli.oksanen@aalto.fi>
2023-01-11 20:45:36 +0200

    add index.css to prevent unnecessary margin on Firefox

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './reducers/store'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
